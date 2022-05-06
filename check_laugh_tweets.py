b8337d5708e6f3ddff827d6321010ea1ec96104f
Malcolm Anderson <malcolminyo@gmail.com>
2022-05-06 09:32:08 -0700

    Allow 'Ah ah ah!' tweets to be plotted against themselves, instead of against time

--- check_laugh_tweets.py
+++ check_laugh_tweets.py
@@ -1,18 +1,19 @@
 from pickle import load, dump
 from typing import List
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+
+plot_against_self = True
 
 distilled: List[dict]
 with open('distilled_tweets', 'rb') as f:
     distilled = [t for t in load(f) if t['hasLaugh']]
 
 plt.plot(
-    [t['date'] for t in distilled],
+    [t['number' if plot_against_self else 'date'] for t in distilled],
     [t['number'] for t in distilled],
     'r.',
     markersize=2)
 plt.title('Count von Count Tweets with \'Ah ah ah!\' versus Timestamp')
-plt.xlabel('Time when he went \'Ah ah ah!\'')
+plt.xlabel(f'{"Number where" if plot_against_self else "Time when"} he went \'Ah ah ah!\'')
 plt.ylabel('Number where he went \'Ah ah ah!\'')
 plt.show()
